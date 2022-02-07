1596bebb6e54d6f5292cba6c1c60e355476f6bb1
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-02-07 10:51:44 -0700

    commit 2

---
+++ practiceone/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class practiceone
+{
+    public interface IClub
+    {
+        string Badge();
+        string Colors();
+        string Players();
+        string Location();
+    }
+    public class RealSaltLake : IClub
+    {
+        public string Badge()
+        {
+            return "sheild";
+        }
+        public string Colors()
+        {
+            return "Claret & Cobalt";
+        }
+        public string Players()
+        {
+            return "Player 1 -- Player 11";
+        }
+        public string Location()
+        {
+            return "Salt Lake City";
+        }
+    }
+    public static void Main()
+    {
+        RealSaltLake rsl = new RealSaltLake();
+        rsl.Badge();
+        Console.WriteLine(rsl.Badge);
+    }
+}
