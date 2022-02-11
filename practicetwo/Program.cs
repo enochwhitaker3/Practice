19cf9617e45e1e4668d0c187357df8fd3fe4338d
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-02-10 22:22:31 -0700

    Enum practice

---
+++ practicetwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace practicetwo;
+class Program
+{
+    enum soccerPlayer
+    {
+        academyPlayer,
+        unSigned,
+        underContract,
+        freeAgent, 
+        retired
+    }
+    
+    public static void Main(string[] args)
+    {
+        soccerPlayer iniesta;
+        iniesta = soccerPlayer.retired;
+        Console.WriteLine(iniesta);
+    }
+}
